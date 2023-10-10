831c53c7a27c8ede7e66da6597c3dc4f1e6ada04
shwoghk14 <shwoghk14@gmail.com>
2023-10-10 21:11:44 +0900

    Apply .gitignore.

--- SwiftUI-Business-Card/Business Card/Business Card/ContentView.swift
+++ SwiftUI-Business-Card/Business Card/Business Card/ContentView.swift
@@ -18,7 +18,16 @@ struct ContentView: View {
 struct NamePartView: View {
     var body: some View {
         VStack {
-            
+            Image(systemName: "hand.thumbsup.fill")
+                .resizable()
+                .frame(width: 45, height: 45)
+                .foregroundStyle(.green)
+            Text("노재화 (Jaehwa Noh)")
+                .font(.system(size: 28))
+                .fontWeight(.bold)
+                .padding(.bottom, 8)
+            Text("세계 최고 모바일 앱 개발자\n(The World best Mobile Application developer)")
+                .multilineTextAlignment(.center)
         }
     }
 }
@@ -29,6 +38,10 @@ struct InformationPartView: View {
             
         }
     }
+}
+
+#Preview {
+    NamePartView()
 }
 
 #Preview {

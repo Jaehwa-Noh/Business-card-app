43e21fd97673e6c0701abf314abb6d8f952993cc
shwoghk14 <shwoghk14@gmail.com>
2023-10-10 21:01:57 +0900

    Init Project.

---
+++ SwiftUI-Business-Card/Business Card/Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Jaehwa Noh on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
